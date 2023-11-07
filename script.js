439aca09b48c4fcc3f07d3a5663b2553a515f26e
Damon Huynh <dhuynh10072007@gmail.com>
2023-11-07 09:23:44 -0800

    Add functions for basic operations, Add operate()
    Operate is there to do all the math and check which operator the user
    chose. The basic operation functions add(),subtract(), etc are there to
    clean up the code and not clutter the operate function with a bunch of
    math

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-function operate(){
-    
+function operate(num1, num2, operator){
+    switch (operator){
+        case "+":
+            return add(num1, num2);
+
+        case "-":
+            return subtract(num1, num2);
+
+        case "x":
+            return multiply(num1, num2);
+
+        case "÷":
+            return divide(num1, num2);
+            
+    }
+
+
 }
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+console.log(operate(4, 5, "x"));
