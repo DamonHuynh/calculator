cbb4eac22216d37cdf86dd2155090c7dba3e6544
Damon Huynh <dhuynh10072007@gmail.com>
2023-11-07 11:00:10 -0800

    Add num and op classes for buttons; displayValues()
    num and op classes are there to easily get references to the DOM in the
    js. displayValues is there to add event listeners onto the buttons and
    to send values to operate(), and display them

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function operate(num1, num2, operator){
+function operate(num1, operator, num2){
     switch (operator){
         case "+":
             return add(num1, num2);
@@ -33,4 +33,47 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-console.log(operate(4, 5, "x"));
+const numBtns = document.querySelectorAll(".nums");
+const opBtns = document.querySelectorAll(".ops");
+const display = document.getElementById("display");
+
+function displayValues(){
+    let value = "";
+    let operator = "";
+    let equation = [];
+    let temp;
+    let result;
+
+    numBtns.forEach(btn => {
+        btn.addEventListener("click",()=> {
+            value += btn.value;
+            console.log(value);
+            console.log(equation);
+            if (equation.length == 2){
+                result = (operate(Number(equation[0]), equation[1], Number(value)));
+                display.textContent = result
+                equation.pop();
+                equation.pop();
+                equation.push(result);
+                
+            }
+        });
+    }); 
+    
+    opBtns.forEach(btn => {
+        btn.addEventListener("click",()=> {
+            temp = value;
+            value = "";
+            operator = btn.value;
+            console.log(operator);
+            if (!result){
+                equation.push(temp);
+            }
+            equation.push(operator);
+            console.log(equation);
+
+        });
+    }); 
+}
+
+displayValues();
